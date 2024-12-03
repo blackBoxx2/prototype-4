aadd5ea94c6ead1f55874baaabb8f9defeed15ac
Michael Amyotte <twinblackbirds@gmx.com>
2024-12-02 23:24:39 -0500

    navbar styling changes, add breadcrumbs styling, add breadcrumbs prototype

--- scripts/breadcrumbs.js
+++ scripts/breadcrumbs.js
@@ -1,19 +1,27 @@
 // automatically set the breadcrumbs for each page
 if (document.readyState !== "loading") { 
-  initBC();
+    initBC();
 } else {
   document.addEventListener("DOMContentLoaded", function () {
     initBC();
   });
 }
 
-function initBC() {
+async function initBC() {
+  await new Promise((resolve) => { // wait for nav first
+    if (document.querySelector("nav") != null) {resolve()} 
+    else {setTimeout(resolve, 10)}
+  });
   const path = window.location.pathname;
   const pathArray = path.split("/");
   const div = document.createElement("div");
-  div.classList.add("bc-container");
+  div.id = "bc-container";
+  const title = document.createElement("span");
+  title.id = "bc-title";
+  title.innerHTML = "You are here:";
+  div.appendChild(title);
   const el = document.createElement("ul");
-  el.classList.add("breadcrumbs");
+  el.id = "breadcrumbs";
   let breadcrumbPath = "";
   for (let i = 0; i < pathArray.length; i++) {
       if (pathArray[i] === "") {
@@ -21,14 +29,15 @@ function initBC() {
       }
       breadcrumbPath += "/" + pathArray[i];
       let li = document.createElement("li");
+      li.classList.add("crumb");
       let a = document.createElement("a");
       a.href = breadcrumbPath;
-      a.innerHTML = pathArray[i];
+      a.innerHTML = pathArray[i] + "⏎";
       li.appendChild(a);
       el.appendChild(li);
   }
-  let header = document.getElementById("header");
+  let nav = document.querySelector("nav");
   div.appendChild(el);
-  header.insertAdjacentElement("afterend", div);
+  nav.insertAdjacentElement("afterend", div);
   console.log('fired');
 }
