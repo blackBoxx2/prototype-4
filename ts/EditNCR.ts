973a5c78a46afb5a2879d761ee10c2ca5e04196e
Carter Williams <cwilliams54@ncstudents.niagaracollege.ca>
2024-12-04 14:07:09 -0500

    Fixed broken pages. Edit still missing select option values dynamically appearing

--- ts/EditNCR.ts
+++ ts/EditNCR.ts
@@ -2,20 +2,26 @@ import { DatabaseLib } from "./database";
 import * as $ from "jquery";
 import { Models } from "./Models";
 
-$(document).ready(function() {
+$(function() {
     const selected = localStorage.getItem('selectedNcrId');
     //Setup dropdown lists:
     const descProductOptions = document.querySelector("descriptionProduct") as HTMLSelectElement;
     var db = DatabaseLib.Database.get();
+    function formatDateString(dateString: string): string {
+        const date = new Date(dateString);
+        return date.toISOString().split('T')[0]; //yyyy-MM-dd format
+      }
     if(selected){
         const selectedID = Number(selected)
         //only show selected ncrlog
         const selectedNCR = db.GetNCRByNumber(selectedID);
-
+        const selectedQA = db.GetQAByID(Number(selectedNCR.QualityAssuranceID));
+        const selectedEng = db.GetENGByID(Number(selectedNCR.EngineeringID));
+        const selectedPurch = db.GetPURByID(Number(selectedNCR.PurchasingID));
         //get all fields
         const fields = { 
             ncrNumberField: document.getElementById("edit-title") as HTMLHeadingElement,
-            processApplicable: document.getElementById("ipa") as HTMLInputElement,
+            processApplicable: document.getElementById("ipa") as HTMLSelectElement,
             descriptionProduct: document.getElementById("descriptionProduct") as HTMLSelectElement,
             supplierName: document.getElementById("supplierName") as HTMLSelectElement,
             productNum: document.getElementById("productNum") as HTMLInputElement,
@@ -37,33 +43,74 @@ $(document).ready(function() {
             nameOfEngineer: document.getElementById("nameOfEngineer") as HTMLInputElement,
             revisionDate: document.getElementById("revisionDate") as HTMLInputElement,
             engineering: document.getElementById("engineering") as HTMLInputElement,
-            engineeringDate: document.getElementById("engineeringDate") as HTMLInputElement
+            engineeringDate: document.getElementById("engineeringDate") as HTMLInputElement,
+             //purchasing
+            decision: document.getElementById("decision") as HTMLSelectElement,
+            carRaised: document.getElementById("carRaised") as HTMLInputElement,
+            carNo: document.getElementById("carNo") as HTMLInputElement,
+            followUpRequired: document.getElementById("followUpRequired") as HTMLInputElement,
+            followUpType: document.getElementById("followUpType") as HTMLInputElement,
+            signedBy: document.getElementById("signedBy") as HTMLInputElement,
+            dateSigned: document.getElementById("dateSigned") as HTMLInputElement,
+            reInspectAcceptable: document.getElementById("reInspectAcceptable") as HTMLInputElement,
+            newNCRNumber: document.getElementById("newNCRNumber") as HTMLInputElement,
+            qualityDept: document.getElementById("qualityDept") as HTMLInputElement,
+            signedByUser: document.getElementById("signedByUser") as HTMLInputElement
         };
         //set all fields
         if(selectedNCR){
-            fields.ncrNumberField.innerHTML = "Editing NCR: " + String(selectedNCR.NCRNumber);
-            fields.processApplicable.value = String(selectedNCR.QualityAssurance.Process);
-            fields.descriptionProduct.value = String(selectedNCR.QualityAssurance.ItemDescription);
-            fields.supplierName.value = String(selectedNCR.QualityAssurance.Process);
-            fields.productNum.valueAsNumber = Number(selectedNCR.QualityAssurance.ProductNo);
-            fields.salesOrderNum.valueAsNumber = Number(selectedNCR.QualityAssurance.OrderNo);
-            fields.quantityRec.valueAsNumber = Number(selectedNCR.QualityAssurance.QuantityReceived);
-            fields.quantityDef.valueAsNumber = Number(selectedNCR.QualityAssurance.QuantityDefective);
-            fields.descriptionDefect.value = String(selectedNCR.QualityAssurance.DefectDescription);
-            fields.chkNonconforming.checked = true;
-            fields.qaName.value = String(selectedNCR.QualityAssurance.SignedBy);
-            fields.qaDate.valueAsDate = new Date(selectedNCR.QualityAssurance.DateSigned);
-            // fields.qaPhoto?.innerHTML = String(selectedNCR.QualityAssurance.);
-            fields.selReview.value = String(selectedNCR.Engineering?.Review);
-            fields.disposition.value = String(selectedNCR.Engineering?.Disposition);
-            fields.requireNotification.checked = Boolean(selectedNCR.Engineering?.NotifyCustomer);
-            fields.requireUpdating.checked = Boolean(selectedNCR.Engineering?.UpdateDrawing);
-            fields.originalRevNum.value = String(selectedNCR.Engineering?.OriginalRevNumber);
-            fields.updatedRevNum.value = String(selectedNCR.Engineering?.LatestRevNumber);
-            fields.nameOfEngineer.value = String(selectedNCR.Engineering?.SignedBy);
-            fields.revisionDate.valueAsDate = new Date(String(selectedNCR.Engineering?.DateSigned));
-            fields.engineering.value = String(selectedNCR.Engineering?.ID); //???
-            fields.engineeringDate.valueAsDate = new Date(String(selectedNCR.Engineering?.DateSigned));
+            //Helper method to format the dates the way html wants it
+            const selectedProcessText = fields.processApplicable.options[fields.processApplicable.selectedIndex].text;
+
+            let selectedProcessValue = fields.processApplicable;
+            let optionValue: number = -1; //default "select" index
+            if (selectedProcessText === "Supplier / Rec. Insp.") {
+                optionValue = 0;
+            } else if (selectedProcessText === "WIP (Production Order)") {
+                optionValue = 1;
+            }
+            
+            
+
+            //set fields
+            fields.ncrNumberField.innerHTML = String(selectedNCR.NCRNumber);
+            fields.processApplicable.selectedIndex = optionValue;
+            fields.descriptionProduct.value = selectedQA.ItemDescription;
+            fields.supplierName.selectedIndex = Number(selectedQA.SupplierID);
+            fields.productNum.value = String(Number(selectedQA.ProductNo));
+            fields.salesOrderNum.value = String(selectedQA.OrderNo);
+            fields.quantityRec.value = String(selectedQA.QuantityReceived);
+            fields.quantityDef.value = String(selectedQA.QuantityDefective);
+            fields.descriptionDefect.value = String(selectedQA.DefectDescription);
+            fields.chkNonconforming.value = "True";
+            fields.qaName.value = String(selectedQA.SignedByUser);
+            fields.qaDate.value = formatDateString(String(selectedQA.DateSigned));
+            // fields.qaPhoto?.value = String(selectedQA.);
+
+            fields.selReview.selectedIndex = Models.Review[selectedEng.Review];
+            fields.disposition.value = String(selectedEng.Disposition);
+            fields.requireNotification.value = String(selectedEng.NotifyCustomer);
+            fields.requireUpdating.value = String(selectedEng.UpdateDrawing);
+            fields.originalRevNum.value = String(selectedEng.OriginalRevNumber);
+            fields.updatedRevNum.value = String(selectedEng.LatestRevNumber);
+            fields.nameOfEngineer.value = String(selectedEng.SignedByUser);
+            fields.revisionDate.value = formatDateString(String(selectedEng.DateSigned));
+            fields.engineering.value = String(selectedEng.ID); //???
+            fields.engineeringDate.value = formatDateString(String(selectedEng.DateSigned));
+            if(selectedPurch){
+                fields.decision.selectedIndex = Models.Decision[selectedPurch.Decision];
+                fields.carRaised.value = String(selectedPurch.CARRaised);
+                fields.carNo.value = String(selectedPurch.CARNo);
+                fields.followUpRequired.value = String(selectedPurch.FollowUpRequired);
+                fields.followUpType.value = String(selectedPurch.FollowUpType);
+                fields.signedBy.value = String(selectedPurch.SignedByUser);
+                fields.dateSigned.value = formatDateString(String(selectedPurch.DateSigned));
+                fields.reInspectAcceptable.value = String(selectedPurch.ReInspectAcceptable);
+                fields.newNCRNumber.value = String(selectedPurch.NewNCRNumber);
+                fields.qualityDept.value = String(selectedPurch.QualityDept);
+                fields.signedByUser.value = String(selectedPurch.SignedByUser);
+            }
+            
         } 
     }
     else{
