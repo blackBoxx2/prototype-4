4d8138e5981f0927ed18e874c8fd6c20984a5e65
Carter Williams <cwilliams54@ncstudents.niagaracollege.ca>
2024-12-04 12:11:20 -0500

    Create, View and Details pages working. Edit changes coming soon

---
+++ ts/CreateNCR.ts
@@ -0,0 +1,210 @@
+import { DatabaseLib } from "./database";
+import * as $ from "jquery"
+import { Models } from "./Models.js";
+
+$(function() { 
+    var btnSubmit = document.querySelector("#btn-submit");
+    var btnSave = this.documentElement.querySelector("#btn-save");
+    //run validation
+    var db = DatabaseLib.Database.get();
+    //get data
+    const fields = {
+        ncrNumberField: document.getElementById("edit-title") as HTMLHeadingElement,
+        processApplicable: document.getElementById("ipa") as HTMLSelectElement,
+        descriptionProduct: document.getElementById("descriptionProduct") as HTMLSelectElement,
+        supplierName: document.getElementById("supplierName") as HTMLSelectElement,
+        productNum: document.getElementById("productNum") as HTMLInputElement,
+        salesOrderNum: document.getElementById("salesOrderNum") as HTMLInputElement,
+        quantityRec: document.getElementById("quantityRec") as HTMLInputElement,
+        quantityDef: document.getElementById("quantityDef") as HTMLInputElement,
+        descriptionDefect: document.getElementById("descriptionDefect") as HTMLTextAreaElement,
+        chkNonconforming: document.getElementById("chkNonconforming") as HTMLInputElement,
+        qaName: document.getElementById("qaName") as HTMLInputElement,
+        qaDate: document.getElementById("qaDate") as HTMLInputElement,
+        qaPhoto: document.getElementById("photo") as HTMLInputElement,
+        //engineering
+        selReview: document.getElementById("selReview") as HTMLSelectElement,
+        disposition: document.getElementById("disposition") as HTMLTextAreaElement,
+        requireNotification: document.getElementById("requireNotification") as HTMLInputElement,
+        requireUpdating: document.getElementById("requireUpdating") as HTMLInputElement,
+        originalRevNum: document.getElementById("originalRevNum") as HTMLInputElement,
+        updatedRevNum: document.getElementById("updatedRevNum") as HTMLInputElement,
+        nameOfEngineer: document.getElementById("nameOfEngineer") as HTMLInputElement,
+        revisionDate: document.getElementById("revisionDate") as HTMLInputElement,
+        engineering: document.getElementById("engineering") as HTMLInputElement,
+        engineeringDate: document.getElementById("engineeringDate") as HTMLInputElement,
+        //purchasing
+        decision: document.getElementById("decision") as HTMLSelectElement,
+        carRaised: document.getElementById("carRaised") as HTMLInputElement,
+        carNo: document.getElementById("carNo") as HTMLInputElement,
+        followUpRequired: document.getElementById("followUpRequired") as HTMLInputElement,
+        followUpType: document.getElementById("followUpType") as HTMLInputElement,
+        signedBy: document.getElementById("signedBy") as HTMLInputElement,
+        dateSigned: document.getElementById("dateSigned") as HTMLInputElement,
+        reInspectAcceptable: document.getElementById("reInspectAcceptable") as HTMLInputElement,
+        newNCRNumber: document.getElementById("newNCRNumber") as HTMLInputElement,
+        qualityDept: document.getElementById("qualityDept") as HTMLInputElement,
+        signedByUser: document.getElementById("signedByUser") as HTMLInputElement
+    };
+    if(btnSave){ //save no validate
+        btnSave.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log("Attempting Save");
+        const selectedProcessText = fields.processApplicable.options[fields.processApplicable.selectedIndex].text;
+        const supplier = db.GetSupplierByID(fields.supplierName.selectedIndex);
+        //set selcted supplier
+        let selectedProcessValue: Models.Process = Models.Process.WIP;
+        if (selectedProcessText === "Supplier / Rec. Insp.") {
+            selectedProcessValue = Models.Process.SupplierOrRecInsp;
+        } else if (selectedProcessText === "WIP (Production Order)") {
+            selectedProcessValue = Models.Process.WIP;
+        }
+        let qaDate = new Date(fields.qaDate.value);
+        let reviewValue: Models.Review;
+        const selectedReview = fields.selReview.value
+        if (selectedReview) { //easily set selected descision to an enum
+            reviewValue = Models.Review[selectedReview as keyof typeof Models.Review];
+        }
+
+        var qa: Models.QualityAssurance = {
+            ID: (db.tables.QualityAssurances.length + 1),
+            Process: selectedProcessValue,
+            SupplierID: supplier.ID,
+            ProductNo: String(fields.productNum?.value),
+            OrderNo: String(fields.salesOrderNum?.value),
+            ItemDescription: String(fields.descriptionProduct?.innerHTML),
+            DefectDescription: String(fields.descriptionDefect?.value),
+            QuantityReceived: Number(fields.quantityRec?.value),
+            QuantityDefective: Number(fields.quantityDef?.value),
+            SignedByUser: 2, //update
+            DateSigned: new Date(qaDate)
+        }
+        var eng: Models.Engineering = {
+            ID: ((db.tables.Engineerings.length + 1)),
+            Review: selectedReview as Models.Review,
+            NotifyCustomer: Boolean(fields?.requireNotification.checked),
+            Disposition: String(fields.disposition?.value),
+            UpdateDrawing: Boolean(fields.requireUpdating?.checked),
+            OriginalRevNumber: String(fields.originalRevNum?.value),
+            LatestRevNumber: String(fields.updatedRevNum?.value),
+            SignedByUser: Number(fields.nameOfEngineer?.value), //update
+            DateSigned: new Date(fields.engineeringDate?.value)
+        }
+        var purch: Models.Purchasing = {
+            ID: Number(db.tables.Purchasings.length + 1),
+            Decision: Models.Decision[fields?.decision.selectedIndex],
+            CARRaised: Boolean(fields?.carRaised.checked),
+            CARNo: String(fields?.carNo.value),
+            FollowUpRequired: Boolean(fields?.followUpRequired.checked),
+            FollowUpType: String(fields?.followUpType.value),
+            SignedByUser: Models.User[fields?.signedByUser.value], //change
+            DateSigned: new Date(fields?.dateSigned.value),
+            ReInspectAcceptable: Boolean(fields.reInspectAcceptable?.checked),
+            NewNCRNumber: Number(fields.newNCRNumber?.value),
+            QualityDept: String(fields.signedByUser?.value)
+        };
+        //Create qa, eng, and purch
+        
+        var msg = db.SaveQA(qa);
+        var msg1 = db.SaveEng(eng);
+        var msg2 = db.SavePurch(purch);
+        var ncrLog: Models.NCRLog = {
+            ID: Number(db.tables.NCRLogs.length + 1),
+            NCRNumber: Number(db.tables.NCRLogs.length + 1),
+            QualityAssuranceID: qa.ID,
+            EngineeringID: eng.ID,
+            PurchasingID: purch.ID,
+            Status: Models.Status.Purchasing //update
+        };
+        
+        var msg3 = db.SaveNCR(ncrLog);
+        console.log(msg);
+        console.log(msg1);
+        console.log(msg2);
+        console.log(msg3);
+        db.SaveChanges();
+        console.log(db.tables);
+    })
+}
+    if(btnSubmit){
+        btnSubmit.addEventListener("click", function(e) {
+            console.log("Attempting SUBMIT");
+            e.preventDefault();
+            const selectedProcessText = fields.processApplicable.options[fields.processApplicable.selectedIndex].text;
+            const supplier = db.GetSupplierByID(fields.supplierName.selectedIndex);
+            let selectedProcessValue: Models.Process = Models.Process.WIP;
+            if (selectedProcessText === "Supplier / Rec. Insp.") {
+                selectedProcessValue = Models.Process.SupplierOrRecInsp;
+            } else if (selectedProcessText === "WIP (Production Order)") {
+                selectedProcessValue = Models.Process.WIP;
+            }
+            let qaDate = new Date(fields.qaDate.value);
+            
+            let reviewValue: Models.Review;
+            const selectedReview = fields.selReview.value
+            if (selectedReview) { //easily set selected descision to an enum
+                reviewValue = Models.Review[selectedReview as keyof typeof Models.Review];
+            }
+        
+            var qa: Models.QualityAssurance = {
+                ID: (db.tables.QualityAssurances.length + 1),
+                Process: selectedProcessValue,
+                SupplierID: supplier.ID,
+                ProductNo: String(fields.productNum?.value),
+                OrderNo: String(fields.salesOrderNum?.value),
+                ItemDescription: String(fields.descriptionProduct?.innerHTML),
+                DefectDescription: String(fields.descriptionDefect?.value),
+                QuantityReceived: Number(fields.quantityRec?.value),
+                QuantityDefective: Number(fields.quantityDef?.value),
+                SignedByUser: 2, //update
+                DateSigned: new Date(qaDate)
+            }
+            var eng: Models.Engineering = {
+                ID: ((db.tables.Engineerings.length + 1)),
+                Review: selectedReview as Models.Review,
+                NotifyCustomer: Boolean(fields?.requireNotification.checked),
+                Disposition: String(fields.disposition?.value),
+                UpdateDrawing: Boolean(fields.requireUpdating?.checked),
+                OriginalRevNumber: String(fields.originalRevNum?.value),
+                LatestRevNumber: String(fields.updatedRevNum?.value),
+                SignedByUser: Number(fields.nameOfEngineer?.value), //update
+                DateSigned: new Date(fields.engineeringDate?.value)
+            }
+            var purch: Models.Purchasing = {
+                ID: Number(db.tables.Purchasings.length + 1),
+                Decision: Models.Decision[fields?.decision.selectedIndex],
+                CARRaised: Boolean(fields?.carRaised.checked),
+                CARNo: String(fields?.carNo.value),
+                FollowUpRequired: Boolean(fields?.followUpRequired.checked),
+                FollowUpType: String(fields?.followUpType.value),
+                SignedByUser: Models.User[fields?.signedByUser.value], //change
+                DateSigned: new Date(fields?.dateSigned.value),
+                ReInspectAcceptable: Boolean(fields.reInspectAcceptable?.checked),
+                NewNCRNumber: Number(fields.newNCRNumber?.value),
+                QualityDept: String(fields.signedByUser?.value)
+            };
+               //Create qa, eng, and purch
+        
+            var msg = db.SaveQA(qa);
+            var msg1 = db.SaveEng(eng);
+            var msg2 = db.SavePurch(purch);
+            var ncrLog: Models.NCRLog = {
+                ID: Number(db.tables.NCRLogs.length + 1),
+                NCRNumber: Number(db.tables.NCRLogs.length + 1),
+                QualityAssuranceID: qa.ID,
+                EngineeringID: eng.ID,
+                PurchasingID: purch.ID,
+                Status: Models.Status.Purchasing //update
+            };
+            
+            var msg3 = db.SaveNCR(ncrLog);
+            console.log(msg);
+            console.log(msg1);
+            console.log(msg2);
+            console.log(msg3);
+            db.SaveChanges();
+            console.log(db.tables);
+        })
+    }
+    
+})
--- ts/DetailsNCR.ts
+++ ts/DetailsNCR.ts
@@ -2,13 +2,16 @@ import { DatabaseLib } from "./database";
 import * as $ from "jquery";
 import { Models } from "./Models";
 
-$(document).ready(function() {
+$(function() {
     const selected = localStorage.getItem('selectedNcrId');
     if(selected){
         var db = DatabaseLib.Database.get();
         const selectedID = Number(selected)
         //only show selected ncrlog
         const selectedNCR = db.GetNCRByNumber(selectedID);
+        const selectedQA = db.GetQAByID(Number(selectedNCR.QualityAssuranceID));
+        const selectedEng = db.GetENGByID(Number(selectedNCR.EngineeringID));
+        const selectedPurch = db.GetPURByID(Number(selectedNCR.PurchasingID));
 
         console.log(selectedNCR);
         //get all fields
@@ -36,33 +39,57 @@ $(document).ready(function() {
             nameOfEngineer: document.getElementById("nameOfEngineer") as HTMLParagraphElement,
             revisionDate: document.getElementById("revisionDate") as HTMLParagraphElement,
             engineering: document.getElementById("engineering") as HTMLParagraphElement,
-            engineeringDate: document.getElementById("engineeringDate") as HTMLParagraphElement
+            engineeringDate: document.getElementById("engineeringDate") as HTMLParagraphElement,
+                  //purchasing
+            decision: document.getElementById("decision") as HTMLParagraphElement,
+            carRaised: document.getElementById("carRaised") as HTMLParagraphElement,
+            carNo: document.getElementById("carNo") as HTMLParagraphElement,
+            followUpRequired: document.getElementById("followUpRequired") as HTMLParagraphElement,
+            followUpType: document.getElementById("followUpType") as HTMLParagraphElement,
+            signedBy: document.getElementById("signedBy") as HTMLParagraphElement,
+            dateSigned: document.getElementById("dateSigned") as HTMLParagraphElement,
+            reInspectAcceptable: document.getElementById("reInspectAcceptable") as HTMLParagraphElement,
+            newNCRNumber: document.getElementById("newNCRNumber") as HTMLParagraphElement,
+            qualityDept: document.getElementById("qualityDept") as HTMLParagraphElement,
+            signedByUser: document.getElementById("signedByUser") as HTMLParagraphElement
         };
         //set all fields
         if(selectedNCR){
             fields.ncrNumberField.innerHTML = String(selectedNCR.NCRNumber);
-            fields.processApplicable.innerHTML = String(selectedNCR.QualityAssurance.Process);
-            fields.descriptionProduct.innerHTML = String(selectedNCR.QualityAssurance.ItemDescription);
-            fields.supplierName.innerHTML = String(selectedNCR.QualityAssurance.Process);
-            fields.productNum.innerHTML = String(selectedNCR.QualityAssurance.ProductNo);
-            fields.salesOrderNum.innerHTML = String(selectedNCR.QualityAssurance.OrderNo);
-            fields.quantityRec.innerHTML = String(selectedNCR.QualityAssurance.QuantityReceived);
-            fields.quantityDef.innerHTML = String(selectedNCR.QualityAssurance.QuantityDefective);
-            fields.descriptionDefect.innerHTML = String(selectedNCR.QualityAssurance.DefectDescription);
+            fields.processApplicable.innerHTML = String(selectedQA.Process);
+            fields.descriptionProduct.innerHTML = String(selectedQA.ItemDescription);
+            fields.supplierName.innerHTML = String(selectedQA.Process);
+            fields.productNum.innerHTML = String(selectedQA.ProductNo);
+            fields.salesOrderNum.innerHTML = String(selectedQA.OrderNo);
+            fields.quantityRec.innerHTML = String(selectedQA.QuantityReceived);
+            fields.quantityDef.innerHTML = String(selectedQA.QuantityDefective);
+            fields.descriptionDefect.innerHTML = String(selectedQA.DefectDescription);
             fields.chkNonconforming.innerHTML = "True";
-            fields.qaName.innerHTML = String(selectedNCR.QualityAssurance.SignedBy);
-            fields.qaDate.innerHTML = String(selectedNCR.QualityAssurance.DateSigned);
-            // fields.qaPhoto?.innerHTML = String(selectedNCR.QualityAssurance.);
-            fields.selReview.innerHTML = String(selectedNCR.Engineering?.Review);
-            fields.disposition.innerHTML = String(selectedNCR.Engineering?.Disposition);
-            fields.requireNotification.innerHTML = String(selectedNCR.Engineering?.NotifyCustomer);
-            fields.requireUpdating.innerHTML = String(selectedNCR.Engineering?.UpdateDrawing);
-            fields.originalRevNum.innerHTML = String(selectedNCR.Engineering?.OriginalRevNumber);
-            fields.updatedRevNum.innerHTML = String(selectedNCR.Engineering?.LatestRevNumber);
-            fields.nameOfEngineer.innerHTML = String(selectedNCR.Engineering?.SignedBy);
-            fields.revisionDate.innerHTML = String(selectedNCR.Engineering?.DateSigned);
-            fields.engineering.innerHTML = String(selectedNCR.Engineering?.ID); //???
-            fields.engineeringDate.innerHTML = String(selectedNCR.Engineering?.DateSigned);
+            fields.qaName.innerHTML = String(selectedQA.SignedByUser);
+            fields.qaDate.innerHTML = String(selectedQA.DateSigned);
+            // fields.qaPhoto?.innerHTML = String(selectedQA.);
+            fields.selReview.innerHTML = String(selectedEng.Review);
+            fields.disposition.innerHTML = String(selectedEng.Disposition);
+            fields.requireNotification.innerHTML = String(selectedEng.NotifyCustomer);
+            fields.requireUpdating.innerHTML = String(selectedEng.UpdateDrawing);
+            fields.originalRevNum.innerHTML = String(selectedEng.OriginalRevNumber);
+            fields.updatedRevNum.innerHTML = String(selectedEng.LatestRevNumber);
+            fields.nameOfEngineer.innerHTML = String(selectedEng.SignedByUser);
+            fields.revisionDate.innerHTML = String(selectedEng.DateSigned);
+            fields.engineering.innerHTML = String(selectedEng.ID); //???
+            fields.engineeringDate.innerHTML = String(selectedEng.DateSigned);
+
+            fields.decision.innerHTML = String(selectedPurch.Decision);
+            fields.carRaised.innerHTML = String(selectedPurch.CARRaised);
+            fields.carNo.innerHTML = String(selectedPurch.CARNo);
+            fields.followUpRequired.innerHTML = String(selectedPurch.FollowUpRequired);
+            fields.followUpType.innerHTML = String(selectedPurch.FollowUpType);
+            fields.signedBy.innerHTML = String(selectedPurch.SignedByUser);
+            fields.dateSigned.innerHTML = String(selectedPurch.DateSigned);
+            fields.reInspectAcceptable.innerHTML = String(selectedPurch.ReInspectAcceptable);
+            fields.newNCRNumber.innerHTML = String(selectedPurch.NewNCRNumber);
+            fields.qualityDept.innerHTML = String(selectedPurch.QualityDept);
+            fields.signedByUser.innerHTML = String(selectedPurch.SignedByUser);
         } 
     }
     else{
